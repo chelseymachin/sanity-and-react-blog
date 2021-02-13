df9a392ec3dc60cf8decce6d7ef3828fa70ffbe2
Chelsey Machin <chelseymachin@gmail.com>
2021-02-13 09:11:55 -0800

    Added useEffect/useState import to file

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
+
 
 export default function AllPosts() {
   const [allPostsData, setAllPosts] = useState(null);
